b61f827cc4b68e3df651c84e3fb64b2e2569bb98
Karl Thomas <thokrl+github@amazon.com>
2020-11-09 13:35:33 -0500

    Corrected mispelling

--- templates/master/default-settings.js
+++ templates/master/default-settings.js
@@ -1,7 +1,7 @@
 var default_settings = {
     ENABLE_DEBUG_RESPONSES: "false",  // Determines whethere to log original English responses and translated responses for debugging
     ES_USE_KEYWORD_FILTERS: "true", // Determines whether to detect keywords from Comprehend when searching for answers
-    ES_KEYWORD_SYNTAX_TYPES: "NOUN,PROPN,VERB,INTJ", //Comprehend will return these parts of speech founf by Amazon Comprehend
+    ES_KEYWORD_SYNTAX_TYPES: "NOUN,PROPN,VERB,INTJ", //Comprehend will return these parts of speech found by Amazon Comprehend
     ES_SYNTAX_CONFIDENCE_LIMIT: ".20", //  Comprehend makes a best effort to determine the parts of speech  in a sentence. The keywords will only be used if the confidence limit is greater than this amount
     ES_MINIMUM_SHOULD_MATCH: "2<75%", // Refer to https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-minimum-should-match.html for more information
     ES_NO_HITS_QUESTION: "no_hits", // The QID of the question when no answers could be found for a user's question
