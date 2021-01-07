93c509ae9c632cf9348271fc7f7ee1aeadd221ca
Karl Thomas <thokrl+github@amazon.com>
2021-01-07 12:49:35 -0500

    Translate CF initial commit #1

--- templates/export/resources.js
+++ templates/export/resources.js
@@ -44,7 +44,7 @@ module.exports=Object.assign(
           },
           "Handler": "index.handler",
           "MemorySize": "1024",
-          "Role": {"Fn::GetAtt": ["ExportRole","Arn"]},
+          "Role": {"Fn::GetAtt": ["ConnectRole","Arn"]},
           "Runtime": "nodejs10.x",
           "Timeout": 300,
           "Tags":[{
@@ -53,6 +53,40 @@ module.exports=Object.assign(
           }]
         }
       },
+      "TranslateCodeVersion":{
+        "Type": "Custom::S3Version",
+        "Properties": {
+            "ServiceToken": { "Ref" : "CFNLambda" },
+            "Bucket": {"Ref":"BootstrapBucket"},
+            "Key": {"Fn::Sub":"${BootstrapPrefix}/lambda/translate.zip"},
+            "BuildDate":(new Date()).toISOString()
+        }
+    },
+    "TranslateLambda": {
+      "Type": "AWS::Lambda::Function",
+      "Properties": {
+        "Code": {
+            "S3Bucket": {"Ref":"BootstrapBucket"},
+            "S3Key": {"Fn::Sub":"${BootstrapPrefix}/lambda/translate.zip"},
+            "S3ObjectVersion":{"Ref":"TranslateCodeVersion"}
+        },
+        "Environment": {
+            "Variables": {
+              outputBucket:{"Ref":"ExportBucket"},
+            }
+        },
+        "Handler": "index.handler",
+        "MemorySize": "1024",
+        "Role": {"Fn::GetAtt": ["ConnectRole","Arn"]},
+        "Runtime": "nodejs10.x",
+        "Timeout": 300,
+        "Tags":[{
+            Key:"Type",
+            Value:"Export"
+        }]
+      }
+    },
+
       "ConnectApiResource": {
         "Type": "AWS::ApiGateway::Resource",
         "Properties": {
@@ -191,6 +225,92 @@ module.exports=Object.assign(
                 "lambda:InvokeFunction"
               ],
               "Resource":[{"Ref":"EsProxyLambda"}]
+          }]
+        }
+      }
+    },
+    "ConnectRole": {
+      "Type": "AWS::IAM::Role",
+      "Properties": {
+        "AssumeRolePolicyDocument": {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Principal": {
+                "Service": "lambda.amazonaws.com"
+              },
+              "Action": "sts:AssumeRole"
+            }
+          ]
+        },
+        "Path": "/",
+        "ManagedPolicyArns": [
+          "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+          {"Ref":"ConnectPolicy"}
+        ]
+      }
+    },
+    "ConnectPolicy": {
+      "Type": "AWS::IAM::ManagedPolicy",
+      "Properties": {
+        "PolicyDocument": {
+          "Version": "2012-10-17",
+          "Statement": [{
+              "Effect": "Allow",
+              "Action": [
+                "s3:*"
+              ],
+              "Resource":[{"Fn::Sub":"arn:aws:s3:::${ExportBucket}*"}]
+          },{
+              "Effect": "Allow",
+              "Action": [
+                "lambda:InvokeFunction"
+              ],
+              "Resource":[{"Ref":"EsProxyLambda"}]
+          }]
+        }
+      }
+    },
+    "TranslateRole": {
+      "Type": "AWS::IAM::Role",
+      "Properties": {
+        "AssumeRolePolicyDocument": {
+          "Version": "2012-10-17",
+          "Statement": [
+            {
+              "Effect": "Allow",
+              "Principal": {
+                "Service": "lambda.amazonaws.com"
+              },
+              "Action": "sts:AssumeRole"
+            }
+          ]
+        },
+        "Path": "/",
+        "ManagedPolicyArns": [
+          "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+          {"Ref":"TranslatePolicy"}
+        ]
+      }
+    },
+    "TranslatePolicy": {
+      "Type": "AWS::IAM::ManagedPolicy",
+      "Properties": {
+        "PolicyDocument": {
+          "Version": "2012-10-17",
+          "Statement": [{
+              "Effect": "Allow",
+              "Action": [
+                "s3:*"
+              ],
+              "Resource":[{"Fn::Sub":"arn:aws:s3:::${ExportBucket}*"}]
+          },{
+              "Effect": "Allow",
+              "Action": [
+                "lambda:InvokeFunction"
+              ],
+              "Resource":[{"Ref":"TranslateLambda"}]
           }]
         }
       }
